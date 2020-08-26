95ba017c3b4677247c91266d3c344e26eeeeb195
Alice Huang (黃聖雯) <10907343@quanta.corp>
2020-08-26 16:00:25 +0800

    added form validation

--- src/App.vue
+++ src/App.vue
@@ -42,6 +42,19 @@
         </div>
 
       </div>
+
+      <div>
+        <h2>Reviews</h2>
+        <p v-if="!reviews.length">There are no reviews yet.</p>
+        <ul>
+          <li v-for="review in reviews">
+            <p>{{ review.name }}</p>
+            <p>Rating: {{ review.rating }}</p>
+            <p>{{ review.review }}</p>
+          </li>
+        </ul>
+      </div>
+
     </div>
     <product-review @review-submitted="addReview"></product-review>
   </body>
--- src/components/ProductReview.vue
+++ src/components/ProductReview.vue
@@ -1,6 +1,13 @@
 <template>
   <form class="review-form" @submit.prevent="onSubmit">
 
+    <p v-if="errors.length">
+      <b>Please correct the following error(s): </b>
+      <ul>
+        <li v-for="error in errors">{{ error }}</li>
+      </ul>
+    </p>
+    
     <p>
       <label for="name">Name: </label>
       <input id="name" v-model="name">
@@ -37,20 +44,28 @@ export default {
     return {
       name: null,
       review: null,
-      rating: null
+      rating: null,
+      errors: []
     }
   },
   methods: {
     onSubmit() {
-      let productReview = {
+      if (this.name && this.review && this. rating) {
+        let productReview = {
         name: this.name,
         review: this.review,
         rating: this.rating
+        }
+        this.$emit('review-submitted', productReview)
+        this.name = null
+        this.review = null
+        this.rating = null
       }
-      this.$emit('review-submitted', productReview)
-      this.name = null
-      this.review = null
-      this.rating = null
+      else {
+        if (!this.name) this.errors.push("Name required.")
+        if (!this.review) this.errors.push("Review required.")
+        if (!this.rating) this.errors.push("Rating required.")
+      }
     }
   }
 }
